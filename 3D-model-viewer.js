58d584b3abec7fcc3212fd8893a6cc52aa0fa9a8
Kusnick <kusnick@imada.sdu.dk>
2025-05-02 12:12:09 +0200

    Test the threejs model innstead of polycam

---
+++ 3D-model-viewer.js
@@ -0,0 +1,89 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://unpkg.com/three@0.169.0/examples/jsm/controls/OrbitControls.js";
+
+let scene, camera, renderer, controls, loader;
+
+export function initThreejs(containerID) {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xdddddd);
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    document.getElementById(containerID).getBoundingClientRect().width /
+      document.getElementById(containerID).getBoundingClientRect().height,
+    0.1,
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(
+    document.getElementById(containerID).getBoundingClientRect().width,
+    document.getElementById(containerID).getBoundingClientRect().height
+  );
+  document.getElementById(containerID)?.appendChild(renderer.domElement);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  const loader = new GLTFLoader();
+  loader.load(
+    "3D sketches/soria-test-360.glb",
+    (gltf) => {
+      scene.add(gltf.scene);
+      camera.position.set(0, 0, 0.0000001); //slight offset is needed
+    },
+    undefined,
+    (error) => {
+      console.error("GLTF load error:", error);
+    }
+  );
+
+  animate();
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect =
+    document.getElementById(containerID).getBoundingClientRect().width /
+    document.getElementById(containerID).getBoundingClientRect().height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(
+    document.getElementById(containerID).getBoundingClientRect().width,
+    document.getElementById(containerID).getBoundingClientRect().height
+  );
+}
+
+function handleFile(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const url = URL.createObjectURL(file);
+    loader.load(
+      url,
+      function (gltf) {
+        clearScene();
+        scene.add(gltf.scene);
+        URL.revokeObjectURL(url);
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
+  }
+}
+
+function clearScene() {
+  for (let i = scene.children.length - 1; i >= 0; i--) {
+    let obj = scene.children[i];
+    if (obj.type === "Group" || obj.type === "Mesh") {
+      scene.remove(obj);
+    }
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
